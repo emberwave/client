94c63d7339cc3acf681327e860e6a5540ec0ba09
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-08-05 12:47:19 -0500

    Update Searchbar.tsx

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 export const Searchbar: FC = () => {
   return (
-    <div className='flex flex-grow justify-center h-[100%] mx-[10px] lg:mx-[50px] my-auto'>
+    <div className='flex flex-grow justify-center h-[100%] px-0 lg:px-[80px] my-auto'>
       <input placeholder='search' className='w-[90%] px-[10px] rounded-[20px] bg-ember-dark-300 border-ember-dark-200 hover:border-ember-dark-100 focus:outline-none border-[2px] duration-100 placeholder:text-ember-dark-100'></input>
     </div>
   );
